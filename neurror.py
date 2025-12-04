ead5cac81c6574c10294715d101909204c03e1b6
Rayana Veron <198809937+RayaVeron@users.noreply.github.com>
2025-12-04 14:23:41 +0000

    commit

--- neurror.py
+++ neurror.py
@@ -2,7 +2,7 @@
 import time
 import json
 import os
-from sentence_transformers import SentenceTransformer 
+from sentence_transformers import SentenceTransformer  # type: ignore
 from sklearn.linear_model import LogisticRegression
 from sklearn.multiclass import OneVsRestClassifier #To support liblinear, from crashing after developer's update
 
@@ -11,45 +11,49 @@ from sklearn.multiclass import OneVsRestClassifier #To support liblinear, from c
 #Before i can use their objects or functions, i have to specify that they must be brought into here
 
 #Before running the code, run this in the terminal: pip install -r dependencies.txt
-model = SentenceTransformer("all-MiniLM-L6-v2") 
+# These imports exist to let me actually use the tools that they offer
+# Before i can use their objects or functions, i have to specify that they must be brought into here
+
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
 # I will use variable model, to store which type of pre-trained model I will apply in my code
 # Make sure to install Python´s SentenceTransformer, import it, and store it before you apply it in your code
 
 
+storage = "nrrordata.json"  # Here I'm storing the file nrrordata.json
+# as an object that my python code can interact with
+# nrrordata.json points you to stored user_inputs
 
-storage = "nrrordata.json" # Here I'm storing the file nrrordata.json
-                           # as an object that my python code can interact with
-                           # nrrordata.json points you to stored user_inputs
+# In order for our json file to write user_inputs into its dictionary list
+data = {"user_input": []}
+# We have to declare it a variable, so our code can interact with it as an object
 
-data = {"user_input": []}  # In order for our json file to write user_inputs into its dictionary list
-                           # We have to declare it a variable, so our code can interact with it as an object
-                           
 
 # These example sentences will be used to train Neurror chat on 4 emotion categories
 # Each emotion category has sentence examples
 # The model i'm using: sentence transformer already has semantic "closeness" scale pre-trained into it.
 # What it doesn't have is: emotion categories, and how much each sentence belongs in said category
 # Which is why you must insert your own example data, that the transformer model will be trained upon
-# It will do that by noticing patterns in the examples. The model uses 384 dimensions per vector to 
-# Describe to itself how close it is to a certain word, category or sentence. 
+# It will do that by noticing patterns in the examples. The model uses 384 dimensions per vector to
+# Describe to itself how close it is to a certain word, category or sentence.
 # Meaning: Every word, sentence or category will have 384 numbers, to describe the object to the model
 # Those dimensions may not do much on their own, but when grouped together the bigger picture allows us to
-# see how close in meaning and semantics, our sentences are. 
+# see how close in meaning and semantics, our sentences are.
 # Your sentences:
 # "I am happy", "I'm sad", "I'm joyful"
 # The positive sentences will have their numeric dimensions be closer: such as 1 and 2
-# But the negative sentences, will be 6. 1 and 2 are closer, than 1 and 6. 
+# But the negative sentences, will be 6. 1 and 2 are closer, than 1 and 6.
 # This is how the model understands sentences, by turning them into integers.
 # 384 embeddings total, do not define, but do indicate our model's token limitations.
-# In this case: 256 tokens roughly = 1,024 characters. 
+# In this case: 256 tokens roughly = 1,024 characters.
 # 1 token = 4 characters
 # It is not to say that it cannot work with large datasets or inputs
-# However, to avoid the program crashing, we must ensure the model processes it chunk by chunk. 
+# However, to avoid the program crashing, we must ensure the model processes it chunk by chunk.
 # Once it reaches 256 tokens, there will be a seperate embedding for that next chunk of text
 # Afterwards, all of the embeddings are compared and averaged out into a seperate embedding, using pooling (math)
 # This approach has negative effects on accuracy, because to split the chunks means to lose context across embeddings
 # and land at a general understanding of the text. Which is why it's best to stay
-# within the scope of 256 tokens per input, to enhance answer accuracy. 
+# within the scope of 256 tokens per input, to enhance answer accuracy.
 joy_sentences = [
     "I am so happy with life, I feel free and light",
     "I am so joyful, ecstatic and excited about everything",
@@ -118,7 +122,7 @@ sadness_sentences = [
     "Ugh I feel sick and I’m in so much pain. Somebody end this",
     "I'm crying and cannot get out of bed",
     "I cannot move, it feels too difficult. I’m so tired"
-    
+
     "I feel so empty inside.",
     "Everything feels heavy.",
     "I can’t stop feeling down.",
@@ -239,7 +243,7 @@ fear_sentences = [
     "Oh no! What if I die alone. I cannot do this, I have to freeze, stay in one place and not move",
     "I need to hide or run away, this cannot happen! This is worst case scenario.",
     "My heart is racing, my thoughts are scattered, I feel broken and all over the place."
-        
+
     "I’m terrified something bad will happen.",
     "My heart won’t stop racing.",
     "I feel like danger is everywhere.",
@@ -297,40 +301,44 @@ fear_sentences = [
 #################################################################
 # Use this to correctly locate our storage file JSON:
 
-#print("json path:", os.path.abspath(storage)) 
+# print("json path:", os.path.abspath(storage))
 ####################################################################
 
 
-
-
-
- ###### PRE-NLP TEXT CLEAN-UP ######
+###### PRE-NLP TEXT CLEAN-UP ######
 
 # Before we process user input and store it in the JSON File
-def clean_text(text): #Define our function as clean_text, that accepts one paremeter: variable text
-            
-    text = text.lower().strip() #### The Information in the text variable will be turned to lower letters, and blank spaces on both ends will be stripped
-    text = text.split() # Every word in this variable will be split into seperate strings
-    text = " ".join(text) # Every string inside the variable will be joined into one string, using the " " as a glue
-            
-    return text # In the end we return the text variable to whoever called this function
-    
-    ######################################  
+def clean_text(text):  # Define our function as clean_text, that accepts one paremeter: variable text
 
-    
-    ##### NEURROR MODEL FUNCTION #####    
-def nrr_chat(clean_input):  # Here we define our function as nrr_chat that accepts a variable as paremeter
-                            # The variable clean_input contains the refined text of user's input
+    # The Information in the text variable will be turned to lower letters, and blank spaces on both ends will be stripped
+    text = text.lower().strip()
+    text = text.split()  # Every word in this variable will be split into seperate strings
+    # Every string inside the variable will be joined into one string, using the " " as a glue
+    text = " ".join(text)
 
-        vector = model.encode(clean_input) # Here we declare that the variable Vector
-                                           # Must take processed input
-                                           # And turn the strings into an encoding
-                                           # Using the rules of our SentenceTransformer model (384-dimensional vector)
-        return vector # Now return the vector to whoever called the nrr_chat function
-    
-    ##################################### 
+    return text  # In the end we return the text variable to whoever called this function
 
-    #This is where the neurror engine exists
+    ######################################
+
+    ##### NEURROR MODEL FUNCTION #####
+# Here we define our function as nrr_chat that accepts a variable as paremeter
+
+
+def nrr_chat(clean_input):
+    # The variable clean_input contains the refined text of user's input
+
+    # Here we declare that the variable Vector
+    vector = model.encode(clean_input)
+    # Must take processed input
+    # And turn the strings into an encoding
+    # Using the rules of our SentenceTransformer model (384-dimensional vector)
+    return vector  # Now return the vector to whoever called the nrr_chat function
+
+    #####################################
+
+    # This is where the neurror engine exists
+
+
 def neurror():
     print(
         "\n"
@@ -340,25 +348,27 @@ def neurror():
         "\n"
         "Tell me about the emotions you went through today.\n"
         "Or type 'x' to exit."
-         "\n"
-         "\n"
-         "------------------\n"
+        "\n"
+        "\n"
+        "------------------\n"
     )
-    
+
    ################## Training Data #####################
-   # This is the point in which we begin to train our data 
+   # This is the point in which we begin to train our data
    # Before we can actually apply neurror chat as users
 
-    emotion_label = [] # Here we will store our vectors (x,y) from the training data
-    emotion_content = [] # Every sentence will be embedded into 384 dimensional vectors (x)
-                         # And every sentence will have its emotion label (y), where it belongs
+    # Here we will store our vectors (x,y) from the training data
+    emotion_label = []
+    # Every sentence will be embedded into 384 dimensional vectors (x)
+    emotion_content = []
+    # And every sentence will have its emotion label (y), where it belongs
 
-    for sentence in joy_sentences:           #A loop that takes every item
-                                             # from the training data variables
-                                             # Stores clean sentences in variable "refined"
-                                             # Per Sentence is turned into an embedding
-                                             # Per item Sentence is added to the emotional_content coordinate
-                                             # Per item (sentence) has a label appended to it
+    for sentence in joy_sentences:  # A loop that takes every item
+        # from the training data variables
+        # Stores clean sentences in variable "refined"
+        # Per Sentence is turned into an embedding
+        # Per item Sentence is added to the emotional_content coordinate
+        # Per item (sentence) has a label appended to it
         refined = clean_text(sentence)
         emb = model.encode(refined)
         emotion_content.append(emb)
@@ -388,71 +398,81 @@ def neurror():
     # Have 384-dimensional vectors listed in them
     # If the number is not 384, something is off:
 
-    #for i in range(4):        # i = 0, 1, 2, 3
-        #print(f"Embedding: vector dimension of item {i} :", len(emotion_content[i]))
-    #print("Total embeddings in all items together:", len(emotion_content))
+    # for i in range(4):        # i = 0, 1, 2, 3
+        # print(f"Embedding: vector dimension of item {i} :", len(emotion_content[i]))
+    # print("Total embeddings in all items together:", len(emotion_content))
     #####################################################
-    
 
     # This is the part where we run the training engine
-    clf = OneVsRestClassifier((LogisticRegression(max_iter=1000, solver="liblinear")))# Here we have our training "classifier", object
-                                                                # 
-                                                               # that is meant to use Logistic Regression method, specifically.
-                                                               # As a way of training upon our data, and learning
-                                                               # How to classify our emotion content, into each emotion label
-                                                               # Here we have set our model to do a max of 1000 iterations before convergence
-                                                               # This means that the model can make a max of 1000 predictions, and adjusting of its weights
-                                                               # For the whole database 
-                                                               # Until we reach convergence: Which is when the model looks at the bigger picture
-                                                               # Of our dataset and says: Is this the closest we can get?
-                                                               # I will also use liblinear, as it is most compatible with small databases
-                                                               # Liblinear uses all examples in the dataset to update its weights during iteration
+    # Here we have our training "classifier", object
+    clf = OneVsRestClassifier((LogisticRegression(max_iter=1000, solver="liblinear"))) #OnevsRest protects it from post developer updates
+    #
+    # that is meant to use Logistic Regression method, specifically.
+    # As a way of training upon our data, and learning
+    # How to classify our emotion content, into each emotion label
+    # Here we have set our model to do a max of 1000 iterations before convergence
+    # This means that the model can make a max of 1000 predictions, and adjusting of its weights
+    # For the whole database
+    # Until we reach convergence: Which is when the model looks at the bigger picture
+    # Of our dataset and says: Is this the closest we can get?
+    # I will also use liblinear, as it is most compatible with small databases
+    # Liblinear uses all examples in the dataset to update its weights during iteration
 
-    clf.fit(emotion_content, emotion_label) # Fit emotion_content/label database into the training model 
+    # Fit emotion_content/label database into the training model
+    clf.fit(emotion_content, emotion_label)
 
     # -----------------------------
     # LOOP
     # -----------------------------
-    while True:            
-        user_input = input("Type here: ")  # As long as user doesn't type x the loop will persist
+    while True:
+        # As long as user doesn't type x the loop will persist
+        user_input = input("Type here: ")
         print("")
         if user_input.lower() == "x":
             print("You have exited the Neurror program.")
             return  # <-- exits ENTIRE FUNCTION
 
-        clean_input = clean_text(user_input) # Refine user's input before sending to the model
+        # Refine user's input before sending to the model
+        clean_input = clean_text(user_input)
 
-        try: #this is the code we are going to try and execute
-            vector = nrr_chat(clean_input) # Run nrr_chat function using user's input, and store it in the vector (It will be stored as list of decimal integers between 0-1)
-            probs = clf.predict_proba([vector])[0] # Predict the probability that this sample, in the row 0, belongs to each one of the possible classes (labels)
-            classes = clf.classes_ # Use attribute .classes to store all the data labels, inside the classes variable
+        try:  # this is the code we are going to try and execute
+            # Run nrr_chat function using user's input, and store it in the vector (It will be stored as list of decimal integers between 0-1)
+            if len(user_input) > 50:
+                 raise ValueError("Not allowed more than 50 characters")
+            vector = nrr_chat(clean_input)
+            # Predict the probability that this sample, in the row 0, belongs to each one of the possible classes (labels)
+            probs = clf.predict_proba([vector])[0]
+            # Use attribute .classes to store all the data labels, inside the classes variable
+            classes = clf.classes_
 
             print("----------------------\n",
-                "Emotion percentages:",
-                "\n"
-                "----------------------\n")
-            for label, pct in zip(classes, probs * 100): #For every label, and its percentage
-                print(f"{label}: {round(pct)}%")  #Turn it into a string 
+                  "Emotion percentages:",
+                  "\n"
+                  "----------------------\n")
+            # For every label, and its percentage
+            for label, pct in zip(classes, probs * 100):
+                print(f"{label}: {round(pct)}%")  # Turn it into a string
                 print("")
 
-        except Exception as fail: # Except if an error arises
-            print("Neurror couldn't understand you, Try again")
-            print(fail)
-            continue # Resume the try-section
+        except ValueError as fail:  # If any error happens
+            print(fail)  # Shows the error message
+            continue  # Go back to the start of the loop
 
-        data["user_input"].append(clean_input) #Store cleaned user input in nrrdata.json file
+        # Store cleaned user input in nrrdata.json file
+        data["user_input"].append(clean_input)
 
-        with open(storage, "w") as f: # We open the file from storage variable, and hold it as an f for writing in data
-            json.dump(data, f, indent=4) # Write the data variable into object f (the real file), use indentation: 4
+        # We open the file from storage variable, and hold it as an f for writing in data
+        with open(storage, "w") as f:
+            # Write the data variable into object f (the real file), use indentation: 4
+            json.dump(data, f, indent=4)
 
-        time.sleep(5) #Allow 5 seconds before next user input
+        time.sleep(5)  # Allow 5 seconds before next user input
         print(
-             "\n",
+            "\n",
             "----------------------\n",
             "Talk to Neurror some more, or type 'x' to exit the program",
-              "\n")
-    
-# START PROGRAM
-neurror() # Call our function so the user always starts in neurror environment, whenever running the program
+            "\n")
 
-    
+
+# START PROGRAM
+neurror()  # Call our function so the user always starts in neurror environment, whenever running the program
